35272f18da4726325110141c187a5d104aef84fd
Kalyan Manna <kalyanmanna439@gmail.com>
2025-09-23 11:29:32 +0530

    updated members details

--- src/components/context/members.ts
+++ src/components/context/members.ts
@@ -1,5 +1,51 @@
 // src/constants/members.ts
 export const members = [
-  { name: "Kalyan Manna", role: "Founder", image: "/assets/member1.jpg" },
-  { name: "Alice", role: "Designer", image: "/assets/member2.jpg" },
+  {
+    id: 1,
+    name: "Naruto Uzumaki",
+    designation: "Software Engineer",
+    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
+    image:
+      "/public/naruto.jpg",
+  },
+  {
+    id: 2,
+    name: "Alisa Kujou",
+    designation: "Product Manager",
+    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
+    image:
+      "/public/alya.jpg",
+  },
+  {
+    id: 3,
+    name: "Monkey D. Luffy",
+    designation: "Data Scientist",
+    desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
+    image:
+      "/public/lufi.jpg",
+  },
+  {
+    id: 4,
+    name: "Makima",
+    designation: "UX Designer",
+    desc: "Ravi has expertise in Node.js and database management, focusing on scalable and secure backend systems. He’s passionate about APIs, microservices, and optimizing server performance.",
+    image:
+      "/public/makima.jpg",
+  },
+  {
+    id: 5,
+    name: "Satoru Gojo",
+    designation: "Soap Developer",
+    desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
+    image:
+      "/public/gojo.jpg",
+  },
+  {
+    id: 6,
+    name: "Miyako Shikimori",
+    designation: "The Explorer",
+    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
+    image:
+      "/shikimori.jpg",
+  },
 ]
