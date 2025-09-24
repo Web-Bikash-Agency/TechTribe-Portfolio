3cd8712b6b7fd8720f12f07fabce1e0120e8e8c3
Kalyan Manna <kalyanmanna439@gmail.com>
2025-09-24 22:35:20 +0530

    feat(context): add founder data

---
+++ src/components/context/founder.ts
@@ -0,0 +1,19 @@
+// src/constants/members.ts
+export const founder = [
+  {
+    id: 1,
+    name: "Naruto Uzumaki",
+    designation: "Software Engineer",
+    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
+    image:
+      "/mikey.jpg",
+  },
+  {
+    id: 3,
+    name: "Monkey D. Luffy",
+    designation: "Data Scientist",
+    desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
+    image:
+      "/draken.jpg",
+  },
+]
