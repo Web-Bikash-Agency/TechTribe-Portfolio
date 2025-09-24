58284162889bb54e4315e9e42ccd121914aed598
Kalyan Manna <kalyanmanna439@gmail.com>
2025-09-24 22:35:12 +0530

    feat(context): add core team data

---
+++ src/components/context/coreTeam.ts
@@ -0,0 +1,43 @@
+// src/constants/members.ts
+export const coreTeam = [
+  {
+    id: 1,
+    name: "Naruto Uzumaki",
+    designation: "Software Engineer",
+    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
+    image:
+      "/naruto.jpg",
+  },
+  {
+    id: 2,
+    name: "Alisa Kujou",
+    designation: "Product Manager",
+    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
+    image:
+      "/alya.jpg",
+  },
+  {
+    id: 3,
+    name: "Monkey D. Luffy",
+    designation: "Data Scientist",
+    desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
+    image:
+      "/lufi.jpg",
+  },
+  {
+    id: 4,
+    name: "Makima",
+    designation: "UX Designer",
+    desc: "Ravi has expertise in Node.js and database management, focusing on scalable and secure backend systems. He’s passionate about APIs, microservices, and optimizing server performance.",
+    image:
+      "/makima.jpg",
+  },
+  {
+    id: 5,
+    name: "Satoru Gojo",
+    designation: "Soap Developer",
+    desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
+    image:
+      "/gojo.jpg",
+  },
+]
