d43e0b89e4ab183e4a51a9181ec7f311c38c9420
Kalyan Manna <kalyanmanna439@gmail.com>
2025-09-26 11:21:59 +0530

    feat(context): update core team data

--- src/components/context/coreTeam.ts
+++ src/components/context/coreTeam.ts
@@ -4,6 +4,7 @@ export const coreTeam = [
     id: 1,
     name: "Naruto Uzumaki",
     designation: "Software Engineer",
+    role: "Tech Lead",
     desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
     image:
       "/naruto.jpg",
@@ -12,6 +13,7 @@ export const coreTeam = [
     id: 2,
     name: "Alisa Kujou",
     designation: "Product Manager",
+    role: "Event Organizer",
     desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
     image:
       "/alya.jpg",
@@ -20,6 +22,7 @@ export const coreTeam = [
     id: 3,
     name: "Monkey D. Luffy",
     designation: "Data Scientist",
+    role: "Marketer",
     desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
     image:
       "/lufi.jpg",
@@ -28,6 +31,7 @@ export const coreTeam = [
     id: 4,
     name: "Makima",
     designation: "UX Designer",
+    role: "Tech Lead",
     desc: "Ravi has expertise in Node.js and database management, focusing on scalable and secure backend systems. He’s passionate about APIs, microservices, and optimizing server performance.",
     image:
       "/makima.jpg",
@@ -36,6 +40,7 @@ export const coreTeam = [
     id: 5,
     name: "Satoru Gojo",
     designation: "Soap Developer",
+    role: "Tech Lead",
     desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
     image:
       "/gojo.jpg",
