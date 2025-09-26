ae04d0b28ef0e87cae017db1f5ad40da9a120d68
Kalyan Manna <kalyanmanna439@gmail.com>
2025-09-27 00:25:10 +0530

    feat(context): update founder data

--- src/components/context/founder.ts
+++ src/components/context/founder.ts
@@ -2,18 +2,24 @@
 export const founder = [
   {
     id: 1,
-    name: "Naruto Uzumaki",
+    name: "Mikey",
     role: "founder",
     designation: "Software Engineer",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto",
     desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
     image:
       "/mikey.jpg",
   },
   {
     id: 2,
-    name: "Monkey D. Luffy",
+    name: "Draken",
     role: "founder",
     designation: "Data Scientist",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto",
     desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
     image:
       "/draken.jpg",
