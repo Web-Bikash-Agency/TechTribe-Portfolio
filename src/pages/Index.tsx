9947f27d06d3adef878ce71a8c314bfff234160a
Kalyan Manna <kalyanmanna439@gmail.com>
2025-09-27 00:25:22 +0530

    feat(page): update Index page content

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,21 +8,22 @@ import ClickSpark from "@/components/ClickSpark"
 
 const Index = () => {
   return (
-    <div>
-      <ClickSpark sparkColor='rgb(57, 255, 20)'
-  sparkSize={10}
-  sparkRadius={15}
-  sparkCount={8}
-  duration={400}>
 
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <About></About>
-      <Highlights></Highlights>
-      <Members></Members>
-      <Footer></Footer>
-  </ClickSpark>
-    </div>
+    <ClickSpark sparkColor='rgb(57, 255, 20)'
+      sparkSize={10}
+      sparkRadius={15}
+      sparkCount={8}
+      duration={400}>
+      <div>
+        <Navbar></Navbar>
+        <Hero></Hero>
+        <About></About>
+        <Highlights></Highlights>
+        <Members></Members>
+        <Footer></Footer>
+
+      </div>
+    </ClickSpark>
   )
 }
 
