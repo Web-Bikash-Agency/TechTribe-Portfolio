f95c104a1e8f84e242ca98b43659780ba5fc3b99
Kalyan Manna <kalyanmanna439@gmail.com>
2025-09-26 11:21:52 +0530

    feat(context): update members data

--- src/components/context/members.ts
+++ src/components/context/members.ts
@@ -4,64 +4,80 @@ export const members = [
     id: 1,
     name: "Naruto Uzumaki",
     designation: "Software Engineer",
-    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
-    image:
-      "/naruto.jpg",
+    desc: "The man in the mirror is only your true competitor.",
+    image: "/naruto.jpg",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto"
   },
   {
     id: 2,
     name: "Alisa Kujou",
     designation: "Product Manager",
-    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
-    image:
-      "/alya.jpg",
+    desc: "Discipline beats talent, obsession beats discipline.",
+    image: "/alya.jpg",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto"
   },
   {
     id: 3,
     name: "Monkey D. Luffy",
     designation: "Data Scientist",
-    desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
-    image:
-      "/lufi.jpg",
+    desc: "Small daily improvements lead to stunning results.",
+    image: "/lufi.jpg",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto"
   },
   {
     id: 4,
     name: "Makima",
     designation: "UX Designer",
-    desc: "Ravi has expertise in Node.js and database management, focusing on scalable and secure backend systems. He’s passionate about APIs, microservices, and optimizing server performance.",
-    image:
-      "/makima.jpg",
+    desc: "Excuses don’t get results; actions do.",
+    image: "/makima.jpg",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto"
   },
   {
     id: 5,
     name: "Satoru Gojo",
     designation: "Soap Developer",
-    desc: "Alice is skilled in building responsive and interactive web interfaces using React and modern CSS frameworks. She specializes in creating smooth user experiences with a strong eye for detail.",
-    image:
-      "/gojo.jpg",
+    desc: "Success is built on consistency, not motivation.",
+    image: "/gojo.jpg",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto"
   },
   {
     id: 6,
     name: "Miyako Shikimori",
     designation: "The Explorer",
-    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
-    image:
-      "/shikimori.jpg",
+    desc: "Pain is temporary, pride is forever.",
+    image: "/shikimori.jpg",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto"
   },
   {
     id: 7,
     name: "Kaoruko Waguri",
     designation: "The Explorer",
-    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
-    image:
-      "/waguri.jpg",
+    desc: "Don’t stop when you’re tired, stop when you’re done.",
+    image: "/waguri.jpg",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto"
   },
   {
     id: 8,
-    name: "astha",
+    name: "Astha",
     designation: "The Explorer",
-    desc: "Sophia is a creative designer with strong skills in wireframing, prototyping, and user research. She excels at crafting visually appealing designs that balance aesthetics with usability.",
-    image:
-      "/astha.jpg",
+    desc: "Your comfort zone is your enemy.",
+    image: "/astha.jpg",
+    linkedin: "https://linkedin.com/in/naruto",
+    github: "https://github.com/naruto",
+    instagram: "https://instagram.com/naruto"
   },
 ]
