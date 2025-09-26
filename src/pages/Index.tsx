4c81ee0f9ca952a527bc37f7001586edf67fd2e9
Kalyan Manna <kalyanmanna439@gmail.com>
2025-09-26 11:23:10 +0530

    feat(page): Added click spark animation

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,20 +1,27 @@
-
 import Navbar from "../components/sections/Navbar"
 import Hero from "../components/sections/Hero"
 import About from "../components/sections/About"
 import Highlights from "../components/sections/Highlights"
 import Footer from "../components/sections/Footer"
 import Members from "../components/sections/Members"
+import ClickSpark from "@/components/ClickSpark"
 
 const Index = () => {
   return (
     <div>
+      <ClickSpark sparkColor='rgb(57, 255, 20)'
+  sparkSize={10}
+  sparkRadius={15}
+  sparkCount={8}
+  duration={400}>
+
       <Navbar></Navbar>
       <Hero></Hero>
       <About></About>
       <Highlights></Highlights>
       <Members></Members>
       <Footer></Footer>
+  </ClickSpark>
     </div>
   )
 }
