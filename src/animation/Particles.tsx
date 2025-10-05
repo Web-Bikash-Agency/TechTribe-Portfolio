174ce64eb5db3bfb4c8bad21aaaa372993037d09
Kalyan Manna <kalyanmanna439@gmail.com>
2025-10-05 11:15:40 +0530

    feat(animation): enhance particle animation visuals

--- src/animation/Particles.tsx
+++ src/animation/Particles.tsx
@@ -78,26 +78,25 @@ const vertex = /* glsl */ `
 
 const fragment = /* glsl */ `
   precision highp float;
-  
-  uniform float uTime;
-  uniform float uAlphaParticles;
-  varying vec4 vRandom;
-  varying vec3 vColor;
-  
-  void main() {
+
+uniform float uTime;
+uniform float uAlphaParticles;
+varying vec4 vRandom;
+varying vec3 vColor;
+
+void main() {
     vec2 uv = gl_PointCoord.xy;
     float d = length(uv - vec2(0.5));
     
     if(uAlphaParticles < 0.5) {
-      if(d > 0.5) {
-        discard;
-      }
-      gl_FragColor = vec4(vColor + 0.2 * sin(uv.yxx + uTime + vRandom.y * 6.28), 1.0);
+        if(d > 0.5) discard;
+        gl_FragColor = vec4(vColor + 0.2 * sin(uv.yxx + uTime + vRandom.y * 6.28), 1.0);
     } else {
-      float circle = smoothstep(0.5, 0.4, d) * 0.8;
-      gl_FragColor = vec4(vColor + 0.2 * sin(uv.yxx + uTime + vRandom.y * 6.28), circle);
+        if(d > 0.5) discard; // remove border effect
+        gl_FragColor = vec4(vColor + 0.2 * sin(uv.yxx + uTime + vRandom.y * 6.28), 1.0);
     }
-  }
+}
+
 `;
 
 const Particles: React.FC<ParticlesProps> = ({
